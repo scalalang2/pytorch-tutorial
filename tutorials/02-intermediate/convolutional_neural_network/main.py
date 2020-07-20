e9861c3b2582f1d5fea92d68b6242ec6ecb92997
DoHyeon Lee <cailhuiris@gmail.com>
2020-07-20 23:52:31 +0900

    Fixed an issue that loss in CNN example is not converged.

--- tutorials/02-intermediate/convolutional_neural_network/main.py
+++ tutorials/02-intermediate/convolutional_neural_network/main.py
@@ -38,13 +38,13 @@ class ConvNet(nn.Module):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
             nn.ReLU(),
+            nn.BatchNorm2d(16),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
+            nn.BatchNorm2d(32),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(7*7*32, num_classes)
         
